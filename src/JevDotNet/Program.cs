219b21e3081cffccd85284afe633ff2b4ded430d
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2026-09-22 19:47:40 +0200

    jevdotnet

---
+++ src/JevDotNet/Program.cs
@@ -0,0 +1,39 @@
+﻿using JevDotNet;
+using JevDotNet.Models;
+using Shared;
+
+Utils.Init("JevDotNet NuGet Demo");
+
+string apiKey = SecretsManager.GetSecrets().TypeSafeApiKey;
+
+JevClient client = new JevClient(apiKey);
+
+JevResponse<MyJevReturnObject> response = await client.EvaluateAsync<MyJevReturnObject>("The device is broken. in need support");
+
+Console.WriteLine("");
+
+public class MyJevReturnObject
+{
+    [JevChoiceQuestion<Category>("What type of category fits this message?")]
+    public JevChoice<Category>? Category { get; set; }
+
+    [JevScoreQuestion<Severity>("How bad is it?")]
+    public JevScore<Severity> Severity { get; set; }
+
+    [JevNoulQuestion("Is the product broken?")]
+    public JevNoul? IsBroken { get; set; }
+}
+
+public enum Severity
+{
+    ALittle,
+    Somewhat,
+    ALot
+}
+
+public enum Category
+{
+    Support,
+    Billing,
+    Other
+}
